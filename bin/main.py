99a70dd73c4a64db3969fd5a3455ce0d51af0bd2
DESKTOP-O88RBUG\azumi <azumi.y.unicom@gmail.com>
2020-09-20 18:44:06 +0900

    ＯＣＲの結果をファイル出力する機能を追加

--- bin/main.py
+++ bin/main.py
@@ -1,6 +1,7 @@
 import sys
 import os 
 import click
+import pathlib
 import logging.config
 
 app_home = os.path.abspath(os.path.join( os.path.dirname(os.path.abspath(__file__)) , ".." ))
@@ -20,19 +21,32 @@ logger = getLogger(__name__)
 # コマンドラインの引数を設定
 @click.command()
 @click.option('--must_arg', '-m', required=True, help="解析画像のファイルパス")
-@click.option('--option_arg', '-o', default='', help="任意引数")
+@click.option('--option_arg', '-o', default='', help="解析結果の出力パス")
 
 def cmd(must_arg, option_arg):
     logger.info("START cmd")
     try:
+        image_file = must_arg
         myOcr = MyOcr()
-        text = myOcr.recognize_image(must_arg)
+        text = myOcr.recognize_image(image_file)
 
-        print(text)
+        # 解析結果の出力
+        if(option_arg == ""):
+            print(text)
+        else:
+            if(Exists_dir(option_arg)):
+                # 新規上書き
+                with open(option_arg, mode='w') as f:
+                    f.write(text)
     except Exception as ex:
         logger.fatal(ex)
         pass
     logger.info("End cmd")
+
+def Exists_dir(file_path):
+    obj_path = pathlib.Path(file_path)
+    return os.path.exists(str(obj_path.parent))
+
 
 # 起点とイメージしやすくするため
 def main():
